cc378c995449301fed181c6d9875b7723f9a0c29
ClaytonDewey <clay@dryadmedia.com>
2024-06-03 14:55:33 -0500

    Async Patterns

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,17 +1,18 @@
-const { readFile, writeFile } = require('fs').promises;
-// const util = require('util');
+const { readFile, writeFile } = require('fs');
+// const util = require('util')
 // const readFilePromise = util.promisify(readFile);
 // const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/second.txt', 'utf8');
+    const first = await readFile('./content/first.txt', 'utf-8');
+    const second = await readFile('./content/second.txt', 'utf-8');
     await writeFile(
       './content/result-mind-grenade.txt',
-      `THIS IS AWESOME : ${first} / ${second}\n`,
+      `THIS IS AWESOME: ${first} ${second}`,
       { flag: 'a' }
     );
+    console.log(first, second);
   } catch (error) {
     console.log(error);
   }
@@ -30,7 +31,6 @@ start();
 //     });
 //   });
 // };
-
 // getText('./content/first.txt')
 //   .then((result) => console.log(result))
 //   .catch((err) => console.log(err));
